a2e01040976e00521650eefdc9c8b9ef6bcfdaa6
szymon <szymon.medykowski@newspoint.pl>
2019-09-28 15:18:53 +0200

    first commit

---
+++ src/main/java/springtaco/taco/TacoApplication.java
@@ -0,0 +1,13 @@
+package springtaco.taco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoApplication.class, args);
+    }
+
+}
